1515abe8c49165317439001afb664754a80e4f65
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-03-22 15:53:50 +0600

    Box Sizing Of Product

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-   const{img,id,name,seller,ratings}=props.product
+   const{img,price,name,seller,ratings}=props.product
     return (
-        <div>
+        <div className='product-container'>
             <img className='image' src={img} alt="" />
+
+           <div>
+           <p className='p-text'>{name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Ratings: {ratings}</small></p>
+           </div>
+
         </div>
     );
 };
