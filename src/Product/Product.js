ed2ee1c120d0a996075f8cab1912792e06496961
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-03-22 17:20:51 +0600

    Font Awesome Icon Added

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
@@ -21,7 +23,8 @@ const Product = ({handleCart,product}) => {
 
            <div>
                <button onClick={()=>handleCart(product)} className='btn-cart'>
-                   <p>Add To Cart</p>
+                   <p className='btn-text'>Add To Cart</p>
+                   <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
                </button>
            </div>
 
